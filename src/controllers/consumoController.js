6db071b1424af5782dde2c8d850b8a705395af0f
unknown <NiczinXD>
2025-07-25 15:54:34 -0300

    Criar um servidor no projeto combu [ Realizar refatoração ]

--- src/controllers/consumoController.js
+++ src/controllers/consumoController.js
@@ -3,13 +3,36 @@ import consumo from "../models/Consumo.js";
 
 class ConsumoController {
     static async busca(req, res){
+        const query = req.query.query;
         try {
-            const lista = await consumo.find({});
-            res.status(200).json(lista);
+            const consumos = await consumo.find({ Dias: query });
+            const resultado = consumos.map(d => ({
+                data: d.data,
+                intervalo: d.Intervalo,
+                kmAcumulado: d.KmAcumulado,
+                dias: d.Dias,
+            }))
+
+            res.json(resultado);
         } catch (error){
             res.status(500).json({ message: "Erro ao buscar dados"});
         }
     }
+
+
+    
+    /*
+    static async buscaPorId(req, res){
+        try {
+            const idConsumo = Number(req.params.idConsumo);
+            const lista = await consumo.findOne({ idConsumo: Number(idConsumo) });
+            res.status(200).json(lista);
+        } catch (error) {
+            res.status(500).json({ message: "Erro ao buscar dados"});
+        }
+    }
+    */
+    
 }
 
 export default ConsumoController;
--- src/routes/consumoRoutes.js
+++ src/routes/consumoRoutes.js
@@ -4,5 +4,7 @@ import ConsumoController from "../controllers/consumoController.js";
 const router = express.Router();
 
 router.get('/busca', ConsumoController.busca);
+//router.get('/busca/:Dias', ConsumoController.buscaPorDias);
+//router.get('/busca/:id', ConsumoController.buscaPorId)
 
 export default router;
