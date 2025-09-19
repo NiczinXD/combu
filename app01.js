6d38ed528b6363a7d0207cf2a202065bbc2e4aa8
rogeriop <pinheiroxrogerio@gmail.com>
2025-09-19 16:24:48 -0300

    Restauração arquivos app01 e Dia outrora deletados

---
+++ app01.js
@@ -0,0 +1,46 @@
+// NOME: app01.js
+// OBJETIVO: Ler um arquivo CSV, processar os dados e salvar em um arquivo JSON.
+// AUTOR: ChatGPT
+// DATA: 16-04-2025
+// VERSÃO: 1.0
+
+import fs from 'fs';
+const caminhoArquivo = './consumo2.csv';
+const pastaResultados = './resultado';
+
+import {Dia}  from "./Dia.js";
+
+function replaceCommaInQuotes(str) {
+    return str.replace(/"[^"]*"/g, (match) => {
+        return match.replace(/,/g, '.');
+    });
+}
+
+let arrayAux = [];
+fs.readFile(caminhoArquivo, 'utf-8', (erro, texto) => {
+    if (erro) {
+        console.error('Erro ao ler o arquivo:', erro.message);
+        return;
+    }
+
+    arrayAux = texto.trim().split('\n');
+
+    const dias = [];
+
+    for (let i = 1; i < arrayAux.length; i++) {
+        const linha = replaceCommaInQuotes(arrayAux[i]);
+        const colunas = linha.split(',');
+
+        const dia = new Dia(colunas);
+        dias.push(dia);
+    }
+
+    const caminhoSaida = `${pastaResultados}/resultado.json`;
+    fs.writeFile(caminhoSaida, JSON.stringify(dias, null, 2), (erro) => {
+        if (erro) {
+            console.error('Erro ao escrever o arquivo:', erro.message);
+            return;
+        }
+        console.log('Arquivo JSON criado com sucesso!');
+    });
+});
